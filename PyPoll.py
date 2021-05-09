29f2fe1965a7b9822ce31095af969f84e1c304a5
Mohammad A. Jilani <82117986+mahmedjilani@users.noreply.github.com>
2021-05-08 22:01:09 -0400

    Put the original code

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
